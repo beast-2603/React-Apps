e00bef0ed17ef2e3663643e12c88f7b1514afe7a
beast-2603 <senapati.sohan31@gmail.com>
2022-01-24 10:42:24 +0530

    not_read

---
+++ clones/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXldug0ygDL9RVdN9EeWq2DPucgAb2CEc",
+    authDomain: "clones-e3605.firebaseapp.com",
+    projectId: "clones-e3605",
+    storageBucket: "clones-e3605.appspot.com",
+    messagingSenderId: "848775602538",
+    appId: "1:848775602538:web:83f9032f3bf7b44a6c4513",
+    measurementId: "G-MK98LC6LZX"
+  };
---
+++ tesla-clone/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <Container>
+            <a>
+                <img src="/images/tesla-written.png" alt="tesla-written" />
+            </a>
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+    
+`
